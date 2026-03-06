fbcad84431a79ba522609d8dd810a7612031a00e
Purv Sakariya <purvsakariya134@gmail.com>
2026-03-06 16:39:21 +0530

    Add closeSvg when side is open

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const sidebar   = document.getElementById('sidebar'),
 navItem  = document.querySelector('.nav-item'),
-hamburger = document.getElementById('hamburgerBtn');
+hamburger = document.getElementById('hamburgerBtn'),
+closeSvg = document.querySelector('.sidebar-close-btn')
 
 function openSidebar() {
   sidebar.classList.add('open');
   document.body.style.overflow = 'hidden';
+  hamburger.style.display = "none" 
+  closeSvg.style.display = "block" 
 }
 
 function closeSidebar() {
   sidebar.classList.remove('open');
   document.body.style.overflow = '';
+  hamburger.style.display = "block" 
+  closeSvg.style.display = "none" 
 }
-
-// Close sidebar on Escape key
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') closeSidebar();
-});
 
 function main(){
 
   hamburger.addEventListener('click', openSidebar);
   navItem.addEventListener('click', closeSidebar);
+  closeSvg.addEventListener('click', closeSidebar);
 
 }
 
