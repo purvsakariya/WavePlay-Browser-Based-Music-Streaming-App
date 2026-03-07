b0ae560c7b6d996fffebfeae15e397c5384767dc
Purv Sakariya <purvsakariya134@gmail.com>
2026-03-07 10:09:49 +0530

    Get songs using fatch

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ navItem  = document.querySelector('.nav-item'),
 hamburger = document.getElementById('hamburgerBtn'),
 closeSvg = document.querySelector('.sidebar-close-btn')
 
+async function getsongs(){
+  let a = await fetch('http://127.0.0.1:3000/songs/')
+  let respone = await a.text()
+  let div = document.createElement('div')
+  div.innerHTML = respone
+  let as = div.getElementsByTagName('a');
+  let songs = []
+  for (const song of as) {
+    if(song.href.endsWith('.mp3')){
+      songs.push(song.href.split('%5C').at(-1))
+    }
+  }
+  return songs;
+}
+
 function openSidebar() {
   sidebar.classList.add('open');
   document.body.style.overflow = 'hidden';
@@ -17,8 +32,10 @@ function closeSidebar() {
   closeSvg.style.display = "none" 
 }
 
-function main(){
+async function main(){
 
+  let songs = await getsongs()
+  console.log(songs);
   hamburger.addEventListener('click', openSidebar);
   navItem.addEventListener('click', closeSidebar);
   closeSvg.addEventListener('click', closeSidebar);
